6b6f81d4ed7c6a3ad2731af5575c75dfe2d6d85d
sourabhrajsingh <sourabh.works@gmail.com>
2018-10-07 13:51:26 +0530

    rework to add multiple fund receivers

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -4,30 +4,38 @@ import"./Pausable.sol";
 
 contract Remittance is Pausable{
 
-    bytes32 public password;
-    address public carol;
+    address[] public receiver;
     mapping(address => uint) public balances;
+    mapping(address => bytes32) public password;
+    address public exchangeAgent;
 
     event LogFundingEscrow(uint amountFunded);
     event LogWippingEscrow(address fundReceiver, uint amountReceived);
 
-    function createPassword(uint passphrase, address _carol) public isActive onlyOwner returns(bool success) {
-        carol = _carol;
-        password = keccak256(abi.encodePacked(passphrase, carol));
+    function createPassword(uint passphrase, address _receiver) public isActive onlyOwner returns(bool success) {
+        receiver.push(_receiver);
+        password[_receiver] = (keccak256(abi.encodePacked(passphrase, _receiver)));
         return true;
     }
 
-    function fundEscrow() payable public isActive returns(bool success) {
-         balances[carol] += msg.value;
+    function exchangeAddress(address assign_exchangeAddress) public isActive onlyOwner returns(bool success) {
+        exchangeAgent = assign_exchangeAddress;
+        return true;
+    }
+
+    function fundEscrow(address _receiver) payable public isActive returns(bool success) {
+        require(balances[_receiver] == 0);
+         balances[_receiver] += msg.value;
         emit LogFundingEscrow(msg.value);
         return true;
     }
 
-    function wipeEscrow(uint _passphrase, address exchangeAgent) public isActive returns(bool success) {
-        require(keccak256(abi.encodePacked(_passphrase, msg.sender)) == password);
+    function wipeEscrow(uint _passphrase) public isActive returns(bool success) {
+        require(keccak256(abi.encodePacked(_passphrase, msg.sender)) == password[msg.sender]);
         emit LogWippingEscrow(msg.sender, balances[msg.sender]);
+
         exchangeAgent.transfer(balances[msg.sender]);
-        balances[carol] = 0;
+        balances[msg.sender] = 0;
         return true;
     }
 
